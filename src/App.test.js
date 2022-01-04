fd44e64382c924f1b27661ea52d9ac4f3a960c40
Carlos Ortiz <cortiz@thoughtworks.com>
2022-01-03 21:28:03 -0500

    Test and code checkbox functionality

--- src/App.test.js
+++ src/App.test.js
@@ -5,12 +5,33 @@ test('button has correct initial color', () => {
   render(<App />)
 
   const colorButton = screen.getByRole('button', { name: 'Change to blue' })
-
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
 
   fireEvent.click(colorButton)
 
   expect(colorButton).toHaveStyle({ backgroundColor: 'blue' })
-
   expect(colorButton.textContent).toBe('Change to red')
+})
+
+test('initial conditions', () => {
+  render(<App />)
+  
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'})
+  expect(colorButton).toBeEnabled()
+
+  const checkBox = screen.getByRole('checkbox')
+  expect(checkBox).not.toBeChecked()
+})
+
+test('checkBox functionality', () => {
+  render(<App />)
+
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'})
+  const checkBox = screen.getByRole('checkbox')
+
+  fireEvent.click(checkBox)
+  expect(colorButton).toBeDisabled()
+
+  fireEvent.click(checkBox)
+  expect(colorButton).toBeEnabled()
 })
